8a2283b168bc6c31ec3f67e472ad9c29deec9ec0
karthikeyamaturidevops <karthikeyamaturidevops@gmail.com>
2025-11-04 15:54:03 +0530

    fix : wrong placeholder

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,9 +4,9 @@
 PERSON1="Trump"
 PERSON2="Karthik"
 
-echo "$$PERSON1: Hello $PERSON2, how are you?"
+echo "$PERSON1: Hello $PERSON2, how are you?"
 echo "$PERSON2: I am fine, thank you! How about you?"
-echo "$$PERSON1: I'm doing great, thanks for asking!"
+echo "$PERSON1: I'm doing great, thanks for asking!"
 echo "$PERSON2: That's good to hear. What brings you here?"
-echo "$$PERSON1: I'm here to discuss trade and international relations."
+echo "$PERSON1: I'm here to discuss trade and international relations."
 echo "$PERSON2: Excellent! Let's have a productive discussion."
