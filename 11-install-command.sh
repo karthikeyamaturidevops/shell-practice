89781d90a66942f04e4d58f52d1935c0411280c9
karthikeyamaturidevops <karthikeyamaturidevops@gmail.com>
2025-11-06 17:01:42 +0530

    add: mysql install command

---
+++ 11-install-command.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install mysql
+
+USERID =$(id -u)
+
+if [ $USERID -nq 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+else
+    echo "you are running with root access."
+
+dnf install mysql -y
