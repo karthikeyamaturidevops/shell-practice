88636d0df6daf36513b0ba9e7ba2079b9856a852
karthikeyamaturidevops <karthikeyamaturidevops@gmail.com>
2025-11-06 17:21:30 +0530

    practice: testing if condtions

--- 11-install-command.sh
+++ 11-install-command.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-USERID=$(id -u)
+USERID=$(id -u) # Get the user ID of the current user
 
-if [ $USERID -ne 0 ]
-then
+# Check if the user is root
+
+if [ $USERID -ne 0 ] 
+then 
     echo "ERROR:: Please run the script as root user."
     exit 1
 else
     echo "SUCCESS:: Running script as root user."
 fi
 
-dnf install mysql -y
+# Install MySQL server
+dnf install mysqlhhyjguyg8gi -y 
+
+if [ $? -eq 0 ]
+then 
+    echo "MySQL server installed successfully."
+else
+    echo "Failed to install MySQL server."
+fi
